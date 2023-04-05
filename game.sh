2c210545e6f1c6297cab82b9b877ca7241f6d850
HarrierPanels <108696009+HarrierPanels@users.noreply.github.com>
2023-04-05 19:37:28 +0300

    Update game.sh

--- game.sh
+++ game.sh
@@ -8,23 +8,23 @@ while [ "$ch" -ne 3 ]; do
          HELP : Hit 2 and enter.
          EXIT : Hit 3 and enter.
          "
-    read -p "Enter your choice : " ch
+    read -rp "Enter your choice : " ch
     if [ "$ch" -eq 1 ];then
     x=0 ;c=0 ;p=0
-    read -p "Enter any number between 0 and 9 : " n
+    read -rp "Enter any number between 0 and 9 : " n
     while [ $c -eq 0 ];
     do
         x=11; r=("$(shuf -i 0-9 -n 10)")
         echo "${r[*]} "; for i in {1..10}; do
-            a[$i]=$i
+            a[i]=$i
         done
         echo "${a[*]} "
-        read -t 5 -p "Enter the index of your number : " x
+        read -t 5 -rp "Enter the index of your number : " x
     if [[ $? -gt 128 ]]; then
     c=1
     break
     fi
-if [ "${r[$(($x))-1]}" -eq "$n" ];then
+if [ "${r[$x-1]}" -eq "$n" ];then
             echo "Great"
     ((p=p+1))
         else
